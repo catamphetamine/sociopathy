f455b4cf6db74be5452ea4d2a771e518ba7d56c9
Nikolay Kuchumov <kuchumovn@gmail.com>
2011-09-29 03:42:40 +0400

    уведомления

--- �������/javascripts/�����/�����������.js
+++ �������/javascripts/�����/�����������.js
@@ -2,8 +2,8 @@ var Message =
 {
 	appearance_duration: 0.5,
 	awareness_enter_time: 1.5,
-	thinking_time: 1.5,
-	length_factor: 0.2,
+	thinking_time: 1.0,
+	length_factor: 0.05,
 	awareness_leave_time: 1.5,
 	disappearance_duration: 0.9,
 	
@@ -15,20 +15,37 @@ var Message =
 		$element.css('z-index', this.z_index)
 	},
 
-	bubble_notifications: function(free_space)
+	bubble_after: function($element)
 	{
-		$('body .message_container').each(function()
+	//alert('bubble scheduled')
+	//alert(Message.state_machine.states.map(function (element) { return element.state }).join(', '))
+		Message.state_machine.transit_to('bubble', (function()
 		{
-			element = $(this)
+			var free_space = $element.height()
+			var messages = $element.nextAll('.message_container')
 			
-			var top = parseInt(element.css('top'))
-			if (isNaN(top))
-				top = 0
+			if (messages.length === 0)
+			{
+				Message.state_machine.next()
+				return
+			}
+			
+			messages.each(function()
+			{
+				element = $(this)
 				
-			element.animate({top: top - free_space}, 1000, 'easeInOutQuad')
+				var top = parseInt(element.css('top'))
+				if (isNaN(top))
+					top = 0
+				
+				element.animate({top: top - free_space}, 1000, 'easeInOutQuad')
+			})
+			
+			$element.remove()
+					
+			setTimeout(function() { Message.state_machine.next() }, 1000)
 		})
-		
-		this.free_space = 0
+		.bind(this))
 	},
 	
 	calculate_occupied_space: function()
@@ -43,8 +60,94 @@ var Message =
 		return occupied_space
 	},
 	
-	message: function(type, text)
+	vanisher: 
 	{
+		messages: [],
+		
+		schedule: function($element)
+		{
+			this.messages.push($element)
+			this.check()
+		},
+		
+		remove: function()
+		{
+			this.messages.shift()
+			this.check()
+		},
+		
+		check: function()
+		{
+			if (this.messages.length === 0)
+				return
+
+			//Message.state_machine.transit_to('vanishing', (function()
+			//{
+				this.vanish(this.messages[0])
+			//})
+			//.bind(this))
+		},
+	
+		vanish: function($element)
+		{
+			var self = this
+			
+			animator.fade_out($element, 
+			{
+				duration: Message.disappearance_duration, 
+				callback: function() 
+				{
+					$element.hide()
+					Message.bubble_after($element)
+					self.remove()
+
+					//Message.state_machine.next()
+				} 
+			})
+		},
+	},
+	
+	state_machine:
+	{
+		states: [],
+		
+		transit_to: function(state, action)
+		{
+			this.states.push({ state: state, action: action })
+				
+			if (this.states.length === 1)
+			{
+				this.states.unshift({ state: 'dummy', action: function() {} })
+				this.next()
+			}
+		},
+		
+		next: function()
+		{
+			this.states.shift()
+			
+			if (this.states.length === 0)
+				return
+			
+			var state = this.states[0]
+			//alert(state.state)
+			state.action()
+		}
+	},
+	
+	message: function(type, text, options)
+	{
+		Message.state_machine.transit_to('show', (function() 
+		{ 
+			this.add_message(type, text, options)
+		})
+		.bind(this))
+	},
+	
+	add_message: function(type, text, options)
+	{
+		options = options || {}
+	
 		var top = this.calculate_occupied_space()
 		
 		var message_container = $('<div class="message_container"/>')
@@ -52,9 +155,13 @@ var Message =
 	
 		var self = this
 
-		var duration = this.awareness_enter_time + this.thinking_time * (1 + text.length * this.length_factor) + this.awareness_leave_time
+		var show_time = this.thinking_time * (1 + text.length * this.length_factor)
+		if (options.время)
+			show_time = options.время
+			
+		var duration = this.awareness_enter_time + show_time + this.awareness_leave_time
 		
-		var message = $('<div class="' + type + '"/>').text(text)
+		var message = $('<div class="' + type + ' non_selectable"/>').text(text)
 		var opacity = message.css('opacity')
 		message.css('opacity', 0)
 		
@@ -67,32 +174,41 @@ var Message =
 			maximum_opacity: opacity,
 			callback: function()
 			{
-				(function() { animator.fade_out(message, { duration: self.disappearance_duration, callback: function() { var free_space = message_container.height(); message_container.remove(); self.bubble_notifications(free_space); } }) }).delay(duration * 1000)
+				(function() 
+				{
+					Message.vanisher.schedule(message_container)
+				})
+				.delay(duration * 1000)
 			}
 		})
+
+		Message.state_machine.next()
 	},
 	
-	info: function(text)
+	info: function(text, options)
 	{
-		this.message('info', text)
+		this.message('info', text, options)
 	},
 	
-	error: function(text)
+	error: function(text, options)
 	{
-		this.message('error', text)
+		this.message('error', text, options)
 	},
 	
-	warning: function(text)
+	warning: function(text, options)
 	{
-		this.message('warning', text)
+		this.message('warning', text, options)
 	}
 }
 
-/*
 $(function()
 {
-	Message.info('проверка')
-	setTimeout(function() { Message.error('проверка 1.5') }, 2000)
-	setTimeout(function() { Message.warning('проверка 2') }, 4000)
+	Message.info('долгое уведомление', { время: 3 })
+	Message.info('уведомление')
+	Message.info('ещё уведомление')
+
+	setTimeout(function() { Message.error('ошибка') }, 2000)
+	setTimeout(function() { Message.warning('предупреждение') }, 4000)
+
+	setTimeout(function() { Message.warning('не добавится во время анимации') }, 7000)
 })
-*/
