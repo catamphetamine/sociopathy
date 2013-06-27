8ad4244da09889eab7f28ed6087d4835d75387b3
Nikolay Kuchumov <kuchumovn@gmail.com>
2013-06-27 10:26:46 +0400

    Update activity monitor.js

--- static resources/javascripts/activity monitor.js
+++ static resources/javascripts/activity monitor.js
@@ -1,58 +1,61 @@
-var Activity = new (new Class
-({
-	Binds: ['report'],
-
-	active: true,
-
-	initialize: function()
-	{
-		this.report.ticking(Configuration.Activity.Interval * 1000)
-	},
-	
-	report: function()
-	{
-		if (!this.active)
-			return
-			
-		this.active = false
-		
-		console.log('Reporting activity on ' + new Date())
-			
-		Ajax.post('/пользователь/присутствие', {})
-	},
-
-	detected: function()
-	{
-		console.log('Activity detected on ' + new Date())
-		
-		this.active = true
-	}
-}))();
-
-(function()
+if (пользователь)
 {
-	$(window).on('focus', function()
+	var Activity = new (new Class
+	({
+		Binds: ['report'],
+	
+		active: true,
+	
+		initialize: function()
+		{
+			this.report.ticking(Configuration.Activity.Interval * 1000)
+		},
+		
+		report: function()
+		{
+			if (!this.active)
+				return
+				
+			this.active = false
+			
+			console.log('Reporting activity on ' + new Date())
+				
+			Ajax.post('/пользователь/присутствие', {})
+		},
+	
+		detected: function()
+		{
+			console.log('Activity detected on ' + new Date())
+			
+			this.active = true
+		}
+	}))();
+	
+	(function()
 	{
-		Activity.detected()
-	})
-
-	$(document).on('keydown', function()
-	{
-		Activity.detected()
-	})
-
-	$(document).on('mousedown', function()
-	{
-		Activity.detected()
-	})
-
-	$(document).on('mousemove', function()
-	{
-		Activity.detected()
-	})
-
-	$(window).on('scroll', function()
-	{
-		Activity.detected()
-	})
-})()
+		$(window).on('focus', function()
+		{
+			Activity.detected()
+		})
+	
+		$(document).on('keydown', function()
+		{
+			Activity.detected()
+		})
+	
+		$(document).on('mousedown', function()
+		{
+			Activity.detected()
+		})
+	
+		$(document).on('mousemove', function()
+		{
+			Activity.detected()
+		})
+	
+		$(window).on('scroll', function()
+		{
+			Activity.detected()
+		})
+	})()
+}
