9e9aa17c8baf42e1a593ad7dffb7d608832dd2e2
Nikolay Kuchumov <kuchumovn@gmail.com>
2013-07-15 15:19:26 +0400

    Update эфир.js

--- static resources/javascripts/эфир.js
+++ static resources/javascripts/эфир.js
@@ -48,7 +48,7 @@ $(document).on('panel_loaded', function()
 			
 		Эфир.следить_за_пользователем(пользователь)
 		
-		эфир = io.connect('http://' + Configuration.Host + ':' + Configuration.Port + '/эфир')
+		эфир = io.connect('http://' + Configuration.Host + ':' + Configuration.Port + '/эфир', { 'force new connection': true })
 		эфир.is_ready = false
 		
 		Эфир.канал = эфир
